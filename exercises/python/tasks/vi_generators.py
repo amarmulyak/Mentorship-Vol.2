f190c3a796a82dd0a592833c874b5a8fa3d57000
Andriy Marmulyak <amarm@softserveinc.com>
2021-04-23 20:59:44 +0300

    Added generator inverse

--- exercises/python/tasks/vi_generators.py
+++ exercises/python/tasks/vi_generators.py
@@ -7,8 +7,14 @@ def square_generator(number):
     return (x ** 2 for x in range(1, number + 1))
 
 
-squares = square_generator(5)
-print(list(squares))
+def inverse_generator(number):
+    return (round(1 / x, 2) for x in range(1, number + 1))
+
+
+# squares = square_generator(5)
+# print(list(squares))
+# inverse = inverse_generator(5)
+# print(list(inverse))
 
 
 # Yield implementation
@@ -18,4 +24,13 @@ def square_generator_yield(number):
 
 
 squares_yield = square_generator_yield(5)
-print(list(squares_yield))
+# print(list(squares_yield))
+
+
+def inverse_generator_yield(number):
+    for x in range(1, number + 1):
+        yield round(1 / x, 2)
+
+
+# inverse_yield = inverse_generator_yield(5)
+# print(list(inverse_generator_yield(5)))
