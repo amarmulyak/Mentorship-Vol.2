cbf997ff41b3f026f74d123cd9b5f6b44335c328
Andriy Marmulyak <amarm@softserveinc.com>
2021-04-26 23:55:38 +0300

    Added singleton example

---
+++ exercises/python/examples/singleton.py
@@ -0,0 +1,36 @@
+class Singleton:
+    _instance = None
+
+    def __new__(cls, *args, **kwargs):
+        if not isinstance(cls._instance, cls):
+            cls._instance = object.__new__(cls, *args, **kwargs)
+        return cls._instance
+
+
+class Singleton2:
+        obj = None  # attribute for storing a single copy
+
+        def __new__(cls, *args, **kwargs):  # class Singleton
+            if cls.obj is None:
+                # If it does not yet exist, then
+                # call __new__ of the parent class
+                cls.obj = object. __new__(cls, *args, **kwargs)
+
+            return cls.obj  # will return Singleton
+
+
+obj = Singleton2()
+obj.Attr = 12
+new_obj = Singleton2()
+print(new_obj.Attr)
+print(obj is new_obj)
+
+
+def decorator_singleton(class_):
+    instances = dict()
+
+    def get_instance(*args, **kwargs):
+        if class_ not in instances:
+            instances[class_] = class_(*args, **kwargs)
+        return instances[class_]
+    return get_instance
