28f6e196448af872848bfd450287c0e64ed15d4a
Andriy Marmulyak <amarm@softserveinc.com>
2021-04-28 10:19:23 +0300

    Added enum to iterator

--- exercises/python/tasks/v_iterator.py
+++ exercises/python/tasks/v_iterator.py
@@ -6,15 +6,21 @@ o Create iterable with two iterators (starts from 1):
     § X(5, “square”) -> 1, 4, 9, 16, 25
     § X(5, “inverse”) -> 1, 0.5, 0.33, 0.25, 0.2
 """
+from enum import Enum, auto
+
+
+class Actions(Enum):
+    SQUARE = auto()
+    INVERSE = auto()
 
 
 class MyIterator:
-    def __init__(self, number, action):
+    def __init__(self, number: int, action: Actions):
         self.number = number
         self.current = 1
         self.action = action
-        if action not in ["square", "inverse"]:
-            raise ValueError("Action should be 'square' or 'inverse'")
+        # if action not in ["square", "inverse"]:
+        #     raise ValueError("Action should be 'square' or 'inverse'")
 
     def __iter__(self):
         return self
@@ -22,10 +28,10 @@ class MyIterator:
     def __next__(self):
         if self.current > self.number:
             raise StopIteration
-        elif self.action == "square":
+        elif self.action == Actions.SQUARE:
             self.current += 1  # I don't like this dupe code
             return (self.current - 1) ** 2
-        elif self.action == "inverse":
+        elif self.action == Actions.INVERSE:
             self.current += 1  # I don't like this dupe code
             return round(1 / (self.current - 1), 2)
         else:
@@ -33,12 +39,12 @@ class MyIterator:
             return self.current - 1
 
 
-# it_sq = MyIterator(5, "square")
-# it_inv = MyIterator(5, "inverse")
-# it_wrong = MyIterator(5, "some")
-# print(list(it_sq))
-# print(list(it_inv))
-# print(list(it_wrong))
+it_sq = MyIterator(5, Actions.SQUARE)
+it_inv = MyIterator(5, Actions.INVERSE)
+it_wrong = MyIterator(5, "some")
+print(list(it_sq))
+print(list(it_inv))
+print(list(it_wrong))
 
 
 # Method 2
@@ -96,9 +102,9 @@ class MyIterable2:
         elif self.type_ == "inverse":
             return InverseIterator(self.number)
 
-
-sq = MyIterable2(10, "square")
-inv = MyIterable2(7, "inverse")
-
-print(list(sq))
-print(list(inv))
+#
+# sq = MyIterable2(10, "square")
+# inv = MyIterable2(7, "inverse")
+#
+# print(list(sq))
+# print(list(inv))
