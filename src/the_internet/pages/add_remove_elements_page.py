3ce25e1c9738c5d196902e7aea5ab6ae31498dec
Andriy Marmulyak <amarm@softserveinc.com>
2021-05-27 22:58:44 +0300

    Refactored add_remove_elements_page

--- src/the_internet/pages/add_remove_elements_page.py
+++ src/the_internet/pages/add_remove_elements_page.py
@@ -1,6 +1,5 @@
 from src.the_internet.pages.base_page import BasePage
 from selenium.webdriver.common.by import By
-from selenium.common.exceptions import TimeoutException
 
 
 class AddRemoveElementsPage(BasePage):
@@ -19,36 +18,15 @@ class AddRemoveElementsPage(BasePage):
     def add_elements(self, number_of_elements: int):
         for number in range(number_of_elements):
             self.add_element()
-        return
 
-    def get_delete_buttons(self, **kwargs):
-        # TODO Шукати елементи методом від драйвера
-        delete_btns = []
-        try:
-            delete_btns = self.find_elements(self.delete_btns_locator, **kwargs)
-        except TimeoutException:
-            pass
-        return delete_btns
-
-    def check_buttons_quantity(self, btns_quantity, **kwargs):
-        # TODO Винести всі ассерт мнетоди в тести, Чек методи до дупи можливо
-        """
-
-        :param btns_quantity: Expected number of buttons
-        :param kwargs: Pass time argument to cut wait time
-        :return: Assertion result
-        """
-        delete_btns = self.get_delete_buttons(**kwargs)
-        assert len(delete_btns) == btns_quantity
+    def get_delete_buttons(self):
+        return self.driver.find_elements(self.delete_btns_locator[0],
+                                         self.delete_btns_locator[1])
 
     def delete_button(self, button_position):
         # TODO Видаляти батон на базі ліста гет_деліт_батонс
-        self.click_on_element(
-            (
-                self.delete_btn_locator[0],
-                self.delete_btn_locator[1].format(button_position),
-            )
-        )
+        self.click_on_element((self.delete_btn_locator[0],
+                               self.delete_btn_locator[1].format(button_position)))
 
     def delete_buttons(self):
         delete_btns = self.get_delete_buttons()
--- tests/the_internet/test_add_remove_elements.py
+++ tests/the_internet/test_add_remove_elements.py
@@ -5,39 +5,39 @@ def test_add_element(driver):
     add_remove_elements = AddRemoveElementsPage(driver)
     add_remove_elements.get_add_remove_elements_page()
     add_remove_elements.add_element()
-    add_remove_elements.check_buttons_quantity(1)
+    assert len(add_remove_elements.get_delete_buttons()) == 1
 
 
 def test_add_elements(driver):
     add_remove_elements = AddRemoveElementsPage(driver)
     add_remove_elements.get_add_remove_elements_page()
     add_remove_elements.add_elements(5)
-    add_remove_elements.check_buttons_quantity(5)
+    assert len(add_remove_elements.get_delete_buttons()) == 5
 
 
 def test_delete_element(driver):
     add_remove_elements = AddRemoveElementsPage(driver)
     add_remove_elements.get_add_remove_elements_page()
     add_remove_elements.add_elements(1)
-    add_remove_elements.check_buttons_quantity(1)
+    assert len(add_remove_elements.get_delete_buttons()) == 1
     add_remove_elements.delete_button(1)
-    add_remove_elements.check_buttons_quantity(0, time=2)
+    assert len(add_remove_elements.get_delete_buttons()) == 0
 
 
 def test_delete_random_elements(driver):
     add_remove_elements = AddRemoveElementsPage(driver)
     add_remove_elements.get_add_remove_elements_page()
     add_remove_elements.add_elements(5)
-    add_remove_elements.check_buttons_quantity(5)
+    assert len(add_remove_elements.get_delete_buttons()) == 5
     add_remove_elements.delete_button(5)
     add_remove_elements.delete_button(3)
-    add_remove_elements.check_buttons_quantity(3)
+    assert len(add_remove_elements.get_delete_buttons()) == 3
 
 
 def test_delete_all_elements(driver):
     add_remove_elements = AddRemoveElementsPage(driver)
     add_remove_elements.get_add_remove_elements_page()
     add_remove_elements.add_elements(5)
-    add_remove_elements.check_buttons_quantity(5)
+    assert len(add_remove_elements.get_delete_buttons()) == 5
     add_remove_elements.delete_buttons()
-    add_remove_elements.check_buttons_quantity(0, time=2)
+    assert len(add_remove_elements.get_delete_buttons()) == 0
