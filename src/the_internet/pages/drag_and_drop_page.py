c02e78c4d2d482e98be0b5e98c4a00bc6d393056
Andriy Marmulyak <amarm@softserveinc.com>
2021-05-29 20:02:42 +0300

    Refactored drag and drop page + added tests

--- src/the_internet/pages/drag_and_drop_page.py
+++ src/the_internet/pages/drag_and_drop_page.py
@@ -1,21 +1,18 @@
 from src.the_internet.pages.base_page import BasePage
 from selenium.webdriver.common.by import By
-from selenium.webdriver import ActionChains
+from seletools.actions import drag_and_drop
 
 
 class DragAndDropPage(BasePage):
-    column_a_locator = (By.XPATH, "//div[@id='column-a']")
-    column_b_locator = (By.XPATH, "//div[@id='column-b']")
+    element_a_locator = (By.XPATH, "//div[@id='column-a']")
+    element_b_locator = (By.XPATH, "//div[@id='column-b']")
 
     def get_drag_and_drop_page(self):
         return self.driver.get("https://the-internet.herokuapp.com/drag_and_drop")
 
-    def drag_and_drop_column_a_to_b(self):
-        # TODO Попробувати бібліотеку drag_and_drop
-        a = self.find_element(self.column_a_locator)
-        b = self.find_element(self.column_b_locator)
-        action_chains = ActionChains(self.driver)
-        # action_chains.drag_and_drop(a, b).perform()
-        action_chains.move_to_element(a).click_and_hold().move_by_offset(
-            280, 150
-        ).release().perform()
+    def drag_and_drop(self, source_locator, target_locator):
+        source = self.find_element(source_locator)
+        target = self.find_element(target_locator)
+        drag_and_drop(self.driver, source, target)
+
+
--- tests/the_internet/test_drag_and_drop.py
+++ tests/the_internet/test_drag_and_drop.py
@@ -2,8 +2,36 @@ from src.the_internet.pages.drag_and_drop_page import DragAndDropPage
 import time
 
 
-def test_drag_and_drop(driver):
+def test_drag_and_drop_a_to_b(driver):
+    # GIVEN the Drag and Drop page is opened
+    #   AND the element A is placed from the left
     drag_and_drop = DragAndDropPage(driver)
     drag_and_drop.get_drag_and_drop_page()
-    drag_and_drop.drag_and_drop_column_a_to_b()
-    time.sleep(5)
+    assert drag_and_drop.element_text_equal(drag_and_drop.element_a_locator, "A")
+
+    # WHEN the element A dragged and dropped on the element B
+    drag_and_drop.drag_and_drop(
+        drag_and_drop.element_a_locator,
+        drag_and_drop.element_b_locator
+    )
+
+    # THEN the element A is placed from the right
+    assert drag_and_drop.element_text_equal(drag_and_drop.element_b_locator, "A")
+
+
+def test_drag_and_drop_b_to_a(driver):
+    # GIVEN the Drag and Drop page is opened
+    #   AND the element B is placed from the right
+    drag_and_drop = DragAndDropPage(driver)
+    drag_and_drop.get_drag_and_drop_page()
+    assert drag_and_drop.element_text_equal(drag_and_drop.element_b_locator, "B")
+
+    # WHEN the element B dragged and dropped on the element A
+    drag_and_drop.drag_and_drop(
+        drag_and_drop.element_b_locator,
+        drag_and_drop.element_a_locator
+    )
+
+    # THEN the element B is placed from the left
+    assert drag_and_drop.element_text_equal(drag_and_drop.element_a_locator, "B")
+
