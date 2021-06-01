c771551736582496c226ae4a6d3539ceb2e79ee9
Andriy Marmulyak <amarm@softserveinc.com>
2021-06-01 12:44:37 +0300

    Added Dynamic Controls page and tests

--- src/the_internet/pages/dynamic_controls_page.py
+++ src/the_internet/pages/dynamic_controls_page.py
@@ -4,10 +4,13 @@ from src.the_internet.pages.base_page import BasePage
 
 class DynamicControlsPage(BasePage):
     checkbox_loctor = (By.ID, "checkbox")
-    add_remove_button_locator = (By.XPATH, "//form[@id='checkbox-example']/button")
-    loader_locator = (By.ID, "loading")
-    message_locator = (By.ID, "message")
-    edit_field_locator = (By.XPATH, "//input[@type='text']")
+    checkbox_add_remove_button_locator = (By.XPATH, "//form[@id='checkbox-example']/button")
+    checkbox_loader_locator = (By.XPATH, "//form[@id='checkbox-example']/div[@id='loading']")
+    checkbox_message_locator = (By.XPATH, "//form[@id='checkbox-example']/p[@id='message']")
+    input_loader_locator = (By.XPATH, "//form[@id='input-example']/div[@id='loading']")
+    input_field_locator = (By.XPATH, "//input[@type='text']")
+    input_enable_disable_btn_locator = (By.XPATH, "//form[@id='input-example']/button")
+    input_message_locator = (By.XPATH, "//form[@id='input-example']/p[@id='message']")
 
     def get_dynamic_controls_page(self):
         return self.driver.get("https://the-internet.herokuapp.com/dynamic_controls")
@@ -16,19 +19,30 @@ class DynamicControlsPage(BasePage):
         return self.element_is_present(self.checkbox_loctor, wait_time)
 
     def click_add_remove_button(self):
-        self.click_on_element(self.add_remove_button_locator)
-        return self.wait_until_invisible(self.loader_locator)
+        self.click_on_element(self.checkbox_add_remove_button_locator)
+        return self.wait_until_invisible(self.checkbox_loader_locator)
 
-    def message_equal(self, text):
-        return self.element_text_equal(self.message_locator, text)
+    def checkbox_message_equal(self, text):
+        return self.element_text_equal(self.checkbox_message_locator, text)
 
-    def add_remove_button_has_caption(self, caption):
-        return self.element_text_equal(self.add_remove_button_locator, caption)
+    def add_remove_button_caption_equal(self, caption):
+        return self.element_text_equal(self.checkbox_add_remove_button_locator, caption)
 
-    def edit_field_is_disabled(self):
-        edit_field = self.find_element(self.edit_field_locator)
+    def input_field_is_disabled(self):
+        edit_field = self.find_element(self.input_field_locator)
         if edit_field.get_attribute("disabled"):
             is_disabled = True
         else:
             is_disabled = False
         return is_disabled
+
+    def enable_disable_button_caption_equal(self, caption):
+        return self.element_text_equal(self.input_enable_disable_btn_locator, caption)
+
+    def click_enable_disable_button(self):
+        self.click_on_element(self.input_enable_disable_btn_locator)
+        return self.wait_until_invisible(self.input_loader_locator)
+
+    def input_message_equal(self, text):
+        return self.element_text_equal(self.input_message_locator, text)
+
--- tests/the_internet/test_dynamic_controls.py
+++ tests/the_internet/test_dynamic_controls.py
@@ -5,19 +5,30 @@ def test_checkbox(driver):
     dynamic_controls = DynamicControlsPage(driver)
     dynamic_controls.get_dynamic_controls_page()
     assert dynamic_controls.checkbox_is_present()
-    assert dynamic_controls.add_remove_button_has_caption("Remove")
+    assert dynamic_controls.add_remove_button_caption_equal("Remove")
 
     dynamic_controls.click_add_remove_button()
     assert not dynamic_controls.checkbox_is_present(wait_time=2)
-    assert dynamic_controls.message_equal("It's gone!")
+    assert dynamic_controls.checkbox_message_equal("It's gone!")
 
     dynamic_controls.click_add_remove_button()
     assert dynamic_controls.checkbox_is_present()
-    assert dynamic_controls.message_equal("It's back!")
+    assert dynamic_controls.checkbox_message_equal("It's back!")
 
 
 def test_edit_field(driver):
     dynamic_controls = DynamicControlsPage(driver)
-    dynamic_controls.get_dynamic_controls_page()
 
-    assert dynamic_controls.edit_field_is_disabled()
+    dynamic_controls.get_dynamic_controls_page()
+    assert dynamic_controls.input_field_is_disabled()
+    assert dynamic_controls.enable_disable_button_caption_equal("Enable")
+
+    dynamic_controls.click_enable_disable_button()
+    assert not dynamic_controls.input_field_is_disabled()
+    assert dynamic_controls.enable_disable_button_caption_equal("Disable")
+    assert dynamic_controls.input_message_equal("It's enabled!")
+
+    dynamic_controls.click_enable_disable_button()
+    assert dynamic_controls.input_field_is_disabled()
+    assert dynamic_controls.enable_disable_button_caption_equal("Enable")
+    assert dynamic_controls.input_message_equal("It's disabled!")
