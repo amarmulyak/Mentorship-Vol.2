e96e09953e67581cb8619b52be42545bdd1635fa
Andriy Marmulyak <amarm@softserveinc.com>
2021-06-02 12:15:36 +0300

    WIP

--- src/the_internet/pages/dynamically_loaded_elements_page1.py
+++ src/the_internet/pages/dynamically_loaded_elements_page1.py
@@ -3,11 +3,32 @@ from src.the_internet.pages.base_page import BasePage
 
 
 class DynamicallyLoadedElementsPage1(BasePage):
-    start_button_locator = (By.XPATH, "//div[@id='start']")
-    dynmaic_text_locator = (By.XPATH, "//div[@id='finish']")
+    # TODO Зробити капсом і протектед
+    start_button_section_locator = (By.XPATH, "//div[@id='start']")
+    start_button_locator = (By.XPATH, "//div[@id='start']/button")
+    dynamic_text_section_locator = (By.XPATH, "//div[@id='finish']")
+    dynamic_text_locator = (By.XPATH, "//div[@id='finish']/h4")
+    loader_locator = (By.ID, "loading")
 
     def get_dynamically_loaded_elements_page1(self):
         return self.driver.get("https://the-internet.herokuapp.com/dynamic_loading/1")
+
+    def start_button_is_displayed(self):
+        return self._element_displayed(self.start_button_section_locator)
+
+    def dynamic_text_is_displayed(self):
+        return self.wait_until_visible(self.dynamic_text_locator)
+        # return self._element_displayed(self.dynamic_text_section_locator)
+
+    def click_start_button(self):
+        self.click_on_element(self.start_button_locator)
+        self.wait_until_invisible(self.loader_locator)
+
+    def start_button_caption_equal(self, text):
+        return self.element_text_equal(self.start_button_locator, text)
+
+    def dynamic_text_equal(self, text):
+        return self.element_text_equal(self.dynamic_text_locator, text)
 
     def _element_displayed(self, locator):
         element = self.find_element(locator)
@@ -17,9 +38,3 @@ class DynamicallyLoadedElementsPage1(BasePage):
         else:
             element_displayed = True
         return element_displayed
-
-    def start_button_is_displayed(self):
-        return self._element_displayed(self.start_button_locator)
-
-    def dynamic_text_is_displayed(self):
-        return self._element_displayed(self.dynmaic_text_locator)
--- tests/the_internet/test_dynamically_loaded_elements1.py
+++ tests/the_internet/test_dynamically_loaded_elements1.py
@@ -7,3 +7,17 @@ def test_dynamically_loaded_elements(driver):
     page.get_dynamically_loaded_elements_page1()
     assert page.start_button_is_displayed()
     assert not page.dynamic_text_is_displayed()
+
+    page.click_start_button()
+    assert not page.start_button_is_displayed()
+    assert page.dynamic_text_is_displayed()
+
+
+def test_elements_text(driver):
+    page = DynamicallyLoadedElementsPage1(driver)
+
+    page.get_dynamically_loaded_elements_page1()
+
+    assert page.dynamic_text_is_displayed()
+    # assert page.start_button_caption_equal("Start")
+    # assert page.dynamic_text_equal("Hello World!")
