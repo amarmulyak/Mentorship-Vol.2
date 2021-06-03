6a55fcde462001806daca6959440b79833b06250
Andriy Marmulyak <amarm@softserveinc.com>
2021-06-03 22:22:35 +0300

    WIP dynamically_loaded_elements2

--- src/the_internet/pages/dynamically_loaded_elements_page1.py
+++ src/the_internet/pages/dynamically_loaded_elements_page1.py
@@ -3,9 +3,9 @@ from src.the_internet.pages.base_page import BasePage
 
 
 class DynamicallyLoadedElementsPage1(BasePage):
-    _START_BUTTON_SECTION = (By.XPATH, "//div[@id='start']")
+    # _START_BUTTON_SECTION = (By.XPATH, "//div[@id='start']")
     _START_BUTTON = (By.XPATH, "//div[@id='start']/button")
-    _DYNAMIC_TEXT_SECTION = (By.XPATH, "//div[@id='finish']")
+    # _DYNAMIC_TEXT_SECTION = (By.XPATH, "//div[@id='finish']")
     _DYNAMIC_TEXT = (By.XPATH, "//div[@id='finish']/h4")
     _LOADER = (By.ID, "loading")
 
@@ -13,7 +13,7 @@ class DynamicallyLoadedElementsPage1(BasePage):
         return self.driver.get(f"{self.path}/dynamic_loading/1")
 
     def start_button_is_visible(self, wait_time=10):
-        return self.element_is_visible(self._START_BUTTON_SECTION, wait_time)
+        return self.element_is_visible(self._START_BUTTON, wait_time)
 
     def dynamic_text_is_visible(self, wait_time=10):
         return self.element_is_visible(self._DYNAMIC_TEXT, wait_time)
@@ -28,11 +28,11 @@ class DynamicallyLoadedElementsPage1(BasePage):
     def dynamic_text(self):
         return self.element_text(self._DYNAMIC_TEXT)
 
-    def _element_displayed(self, locator):
-        element = self.find_element(locator)
-        style = element.get_attribute("style")
-        if style == "display: none;":
-            element_displayed = False
-        else:
-            element_displayed = True
-        return element_displayed
+    # def _element_displayed(self, locator):
+    #     element = self.find_element(locator)
+    #     style = element.get_attribute("style")
+    #     if style == "display: none;":
+    #         element_displayed = False
+    #     else:
+    #         element_displayed = True
+    #     return element_displayed
---
+++ src/the_internet/pages/dynamically_loaded_elements_page2.py
@@ -0,0 +1,5 @@
+from src.the_internet.pages.base_page import BasePage
+
+
+class DynamicallyLoadedElementsPage2(BasePage):
+    pass
