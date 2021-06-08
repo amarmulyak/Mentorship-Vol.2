8f6fbb854fefc403e3ef3c9dd6c3e9135d5e87cd
Andriy Marmulyak <amarm@softserveinc.com>
2021-06-08 17:38:30 +0300

    WIP file upload test2 added method

--- tests/the_internet/test_file_uploader.py
+++ tests/the_internet/test_file_uploader.py
@@ -14,7 +14,8 @@ def test_upload_via_button(driver, cfg):
 
 def test_upload_drag_and_drop(driver, cfg):
     file_uploader = FileUploaderPage(driver, cfg.base_url)
-    file_name = "box.png"
+    # file_name = "box.png"
 
     file_uploader.get_file_uploader_page()
-    file_uploader.choose_file_via_drag_and_drop(f"src/the_internet/pages/{file_name}")
+    dd = file_uploader.find_element(file_uploader.DRAG_DROP_UPLOAD)
+    file_uploader.drop_files(dd, '/home/amarm/repositories/Mentorship-Vol.2/src/the_internet/pages/box.png')
