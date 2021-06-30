fb824d2407fc0895210c6f906a15267b977dd6bf
amarmulyak <a.marmuliak@gmail.com>
2021-06-30 23:05:50 +0300

    WIP docs

--- src/the_internet/pages/challenging_dom_page.py
+++ src/the_internet/pages/challenging_dom_page.py
@@ -1,23 +1,54 @@
+"""
+Challenging Dom module.
+"""
+
 from selenium.webdriver.common.by import By
+
 from src.the_internet.pages.base_page import BasePage
 
 
 class ChallengingDomPage(BasePage):
+    """
+    Class to represent Challenging Dom page.
+    """
+
     _ANSWER = (By.ID, "canvas")
     _ANSWER_TEXT = (By.XPATH, "//script/text()")
     _CELL_DATA = (
         By.XPATH,
-        "//div[@class='large-10 columns']/table/tbody/tr[position()={}]/td[position()=count(//div[@class='large-10 columns']/table/thead/tr/th[contains(., '{}')]/preceding-sibling::th)+1]",
+        "//div[@class='large-10 columns']/table/tbody/tr[position()={}]/"
+        "td[position()=count(//div[@class='large-10 columns']/"
+        "table/thead/tr/th[contains(., '{}')]/preceding-sibling::th)+1]"
     )
 
     def get_challenging_dom_page(self):
+        """
+        Open Challenging Dom page.
+
+        :return: None
+        """
+
         return self.driver.get(f"{self.url}/challenging_dom")
 
     def get_answer(self):
+        """
+        Get canvas
+
+        :return: Result of the script execution
+        """
+
         answer = self.driver.execute_script("return canvas;")
         return answer
 
     def get_data_from_cell(self, row_number, title_name):
+        """
+        Get text from the cell
+
+        :param row_number: Number of the row
+        :param title_name: Name of the column's title
+        :return: Text
+        """
+
         return self.find_element(
             (
                 self._CELL_DATA[0],
