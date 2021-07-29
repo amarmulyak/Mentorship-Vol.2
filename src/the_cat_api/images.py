2860300e46965bafa035dce28ce00b72c47725fc
Andriy Marmulyak <amarm@softserveinc.com>
2021-07-29 19:01:04 +0300

    images api

--- src/the_cat_api/images.py
+++ src/the_cat_api/images.py
@@ -6,5 +6,9 @@ class Images:
     def __init__(self):
         self.endpoint = f"{cfg().the_cat_api.url}/images/search"
 
-    def get_images_search(self):
-        return requests.get(self.endpoint, headers={"x-api-key": f"{cfg().the_cat_api.x_api_key}"})
+    def get_images_search(self, size):
+        params = {}
+        if size:
+            # params = {"size": f"{size}"}
+            params["size"] = size
+        return requests.get(self.endpoint, headers={"x-api-key": f"{cfg().the_cat_api.x_api_key}"}, params=params)
--- tests/the_cat_api/test_image_search.py
+++ tests/the_cat_api/test_image_search.py
@@ -1,7 +1,10 @@
 from src.the_cat_api.images import Images
+import json
 
 
 def test_get_vote():
     images = Images()
-    r = images.get_images_search()
+    r = images.get_images_search(size="full")
+    r_dict = json.loads(r.content)
     a = 1
+
