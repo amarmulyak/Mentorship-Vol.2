435134acaa4ef79fe769bdf2231bcaa5d3fecdfd
Andriy Marmulyak <a.marmuliak@gmail.com>
2022-12-16 00:19:17 +0200

    ajax

--- tests/ajax/ajax.py
+++ tests/ajax/ajax.py
@@ -1,5 +1,26 @@
 payload = "10FA0E00"
 
+field1_data = {
+    '0': 'Low',
+    '1': 'reserved',
+    '2': 'reserved',
+    '3': 'reserved',
+    '4': 'Medium',
+    '5': 'reserved',
+    '6': 'reserved',
+    '7': 'High'
+}
+
+field4_data = {
+    '0': '00',
+    '1': '10',
+    '2': '20',
+    '3': '30',
+    '4': '40',
+    '5': '50',
+    '6': '60',
+    '7': '70'
+}
 
 def convert_hex_bytes_to_bit(hex_data: str):
     scale = 16
@@ -10,47 +31,29 @@ def convert_hex_bytes_to_bit(hex_data: str):
 
 def convert_bit_to_int(bit_data: str):
     scale = 2
+
     return int(bit_data, scale)
 
 
-def convert_byte_1_to_bits(byte: str):
-    bit = convert_hex_bytes_to_bit(byte)
+def convert_bit_to_bit(bit_data: str):
+    scale = 2
+    num_of_bits = 2
 
-    field_1 = bit[:3]
-    field_2 = bit[3]
-    field_3 = bit[4]
-    field_4 = bit[5:]
+    return bin(int(bit_data, scale))[2:].zfill(num_of_bits)
+
+
+def parse_byte_1(byte_1: str):
+    bit = convert_hex_bytes_to_bit(byte_1)
+    field_1_key = str(convert_bit_to_int(bit[:3]))
+    field_4_key = str(convert_bit_to_int(bit[5:]))
+
+    field_1 = field1_data[field_1_key]
+    field_2 = convert_bit_to_bit(bit[3])
+    field_3 = convert_bit_to_bit(bit[4])
+    field_4 = field4_data[field_4_key]
 
     return field_1, field_2, field_3, field_4
 
-def get_field_1(field_1: str):
-    field1_data = {
-        '0': 'Low',
-        '1': 'reserved',
-        '2': 'reserved',
-        '3': 'reserved',
-        '4': 'Medium',
-        '5': 'reserved',
-        '6': 'reserved',
-        '7': 'High'
-    }
-
-    return field1_data[convert_bit_to_int(field_1)]
-
-
-def get_field_4(field_4: str):
-    field4_data = {
-        '0': '00',
-        '1': '10',
-        '2': '20',
-        '3': '30',
-        '4': '40',
-        '5': '50',
-        '6': '60',
-        '7': '70'
-    }
-
-    return field4_data[convert_bit_to_int(field_4)]
 
 def convert_payload_to_bytes(payload: str):
     byte_1 = payload[:2]
@@ -59,3 +62,12 @@ def convert_payload_to_bytes(payload: str):
     byte_4 = payload[6:]
 
     return byte_1, byte_2, byte_3, byte_4
+
+byte_1, byte_2, byte_3, byte_4 = convert_payload_to_bytes(payload)
+
+field_1, field_2, field_3, field_4 = parse_byte_1(byte_1)
+
+print(field_1)
+print(field_2)
+print(field_3)
+print(field_4)
--- tests/ajax/play.py
+++ tests/ajax/play.py
@@ -1,5 +1,7 @@
 payload = "10FA0E00"
 
+print(bytes.fromhex(payload))
+
 my_hexdata = "10"
 scale = 16 ## equals to hexadecimal
 num_of_bits = 8
@@ -10,20 +12,22 @@ res_1 = bin(int(my_hexdata, scale))[2:].zfill(num_of_bits)
 my_binary = "0"
 scale = 2 ## equals to binary
 num_of_bits = 2
-res_2 = bin(int(my_hexdata, scale))[2:].zfill(num_of_bits)
+res_2 = bin(int(my_binary, scale))[2:].zfill(num_of_bits)
 
 # print(res_2)
 
 
-my_hexdata = "100"
+my_binary = "110"
 scale = 2 ## equals to decimal
-res_3 = int(my_hexdata, scale)
+res_3 = int(my_binary, scale)
 
 # print(res_3)
 
-bit = [0, 1, 2, 3, 4, 5, 6, 7]
+# bit = [0, 1, 2, 3, 4, 5, 6, 7]
+# bit = '00001000'
+# bit = '12345678'
 
-print(bit[:3])
-print(bit[3])
-print(bit[4])
-print(bit[5:])
+# print(bit[:3])
+# print(bit[3])
+# print(bit[4])
+# print(bit[5:])
