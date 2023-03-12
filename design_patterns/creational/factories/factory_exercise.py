eaa5fe6c17da96d218f7c1235604c64f422c138a
Andriy Marmulyak <a.marmuliak@gmail.com>
2023-03-12 11:41:12 +0200

    add

--- design_patterns/creational/factories/factory_exercise.py
+++ design_patterns/creational/factories/factory_exercise.py
@@ -1,3 +1,15 @@
+'''
+Factory Coding Exercise
+
+You are given a class called Person. The person has two attributes: id, and name.
+
+Please implement a PersonFactory that has a non-static create_person() method that takes a person's name
+and return a person initialized with this name and an id.
+
+The id of the person should be set as a 0-based index of the object created. So, the first person the factory makes
+should have id=0, second id=1 and so on.
+'''
+
 class Person:
     def __init__(self, id, name):
         self.id = id
