a7696956ef52baa2e54c560664c61dcce4f05c63
Andriy Marmulyak <Andriy.Marmulyak-external@sema4genomics.com>
2023-03-20 09:48:52 +0200

    decorator

--- design_patterns/creational/singleton/singleton_allocator.py
+++ design_patterns/creational/singleton/singleton_allocator.py
@@ -13,8 +13,7 @@ class Database:
 
     def __new__(cls, *args, **kwargs):
         if not cls._instance:
-            cls._instance = super(Database, cls)\
-                .__new__(cls, *args, **kwargs)
+            cls._instance = super(Database, cls).__new__(cls, *args, **kwargs)
 
         return cls._instance
 
