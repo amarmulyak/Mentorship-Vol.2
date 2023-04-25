6d9c7aef0afad2ab72aacfebbe1863cce1b2e731
Andriy Marmulyak <Andriy.Marmulyak-external@sema4genomics.com>
2023-04-25 12:10:25 +0300

    json_path

--- models/marshmallow_dataclass_example.py
+++ models/marshmallow_dataclass_example.py
@@ -1,7 +1,15 @@
-from typing import Dict, Any
-
 from dataclasses import field
+from typing import Dict, Any, List
+
 import marshmallow_dataclass
+from jsonpath_ng.ext import parse
+
+
+def get_json_path(json: Dict[str, Any], json_path: str, allow_empty: bool = False) -> List[Any]:
+    values = [match.value for match in parse(json_path).find(json)]
+    if not allow_empty:
+        assert values, f'Parameter: {json_path} is not found'
+    return values
 
 
 @marshmallow_dataclass.dataclass
@@ -22,10 +30,11 @@ class AlphabetModel(BaseModel):
     c: str = field(metadata={'data_key': 'c'})
 
 
-response = {
-    'a': 'A',
-    'b': 'B',
-    'c': 'C'
+response_json = {
+    '1': {'a': 'A','b': 'B','c': 'C'},
+    '2': {'d': 'D','e': 'E','f': 'F'}
 }
+
+response = get_json_path(response_json, json_path='$."1"')[0]
 
 alphabet = AlphabetModel.load(response)
